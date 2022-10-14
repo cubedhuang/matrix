175de4053a068506546e51d6db2f2795e5d8ce6e
cubedhuang <99danielh@gmail.com>
2022-10-14 19:41:49 -0400

    feat: add transpose

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,13 @@
 </script>
 
 <div class="flex h-screen overflow-hidden">
-	<nav class="w-48 bg-blue-100">
+	<nav class="overflow-y-auto w-48 bg-blue-100">
 		<NavLink href="/">Home</NavLink>
 		<p class="font-bold px-4 py-2 border-y border-black bg-white">
 			Unary Operations
 		</p>
 		<NavLink href="/determinant">Determinant</NavLink>
+		<NavLink href="/transpose">Transpose</NavLink>
 		<NavLink href="/inverse">Inverse</NavLink>
 		<NavLink href="/rref">RREF</NavLink>
 		<NavLink href="/lu">LU Decomposition</NavLink>
@@ -22,7 +23,7 @@
 	</nav>
 
 	<div
-		class="flex-1 flex flex-col items-center gap-4 py-16 px-8 overflow-y-scroll"
+		class="flex-1 flex flex-col items-center gap-4 py-16 px-8 overflow-y-auto"
 	>
 		<slot />
 	</div>
